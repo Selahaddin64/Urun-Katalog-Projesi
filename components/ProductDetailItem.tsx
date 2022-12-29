7e6014ed9b5e28c4c34da5602071808b49e18254
Selahattin Demir <selahattindemir686@gmail.com>
2022-12-29 23:38:50 +0300

    URL was taken from .env and page layout formatted

--- components/ProductDetailItem.tsx
+++ components/ProductDetailItem.tsx
@@ -1,43 +1,50 @@
-
-import { HeartIcon } from '@heroicons/react/outline';
-import Link from 'next/link';
-import React from 'react';
+import { HeartIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import React from "react";
 
 interface Props {
-    product: Product;
+  product: Product;
 }
 
 function ProductDetailItem({ product }: Props) {
-    const url = `https://assignment-api.piton.com.tr${product.image}`
+  const url = `${process.env.NEXT_PUBLIC_IMAGE_URL}${product.image}`;
   return (
     <div>
-        <div className="py-2">
-        <Link href={"/"}>back to products</Link>
+      <div className="py-2">
+        <Link href={"home"}>back to products</Link>
+      </div>
+      <div className="flex grid h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-slate-100 p-8 md:h-[550px] md:w-[2000px] md:grid-cols-4 md:gap-3 md:p-10">
+        <div className="h-fit w-[320px] pl-6 md:col-span-1">
+          <img src={url} alt="" />
+        </div>
+        <div className="ml-12 pt-28">
+          <ul>
+            <li>
+              <h1 className="text-lg font-bold">{product.name}</h1>
+            </li>
+            <li className="pt-6 md:w-[500px]">
+              Description: {product.description}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="card p-5">
+            <div className="ml-52 ">
+              <HeartIcon
+                fill="currentColor"
+                className="float-left h-6 w-6 text-red-600"
+              />{" "}
+              3 likes
+            </div>
+            {/* {product.likes.length} */}
+            <div className="mt-80 h-8 w-32 rounded-full bg-blue-600 text-2xl font-bold text-white">
+              <div className="text-center">{product.price}.00 ₺</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="grid md:grid-cols-4 md:gap-3 flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-slate-100 p-8 md:h-[550px] md:w-[2000px] md:p-10">
-        <div className="md:col-span-1 pl-6 h-fit w-[320px]">     
-            <img src={url} alt="" />
-        </div>
-            <div className='ml-12 pt-28'>
-                <ul>
-                    <li>
-                        <h1 className="text-lg font-bold">{product.name}</h1>
-                    </li>
-                      <li className='pt-6 md:w-[500px]'>Description: {product.description}</li>
-                  </ul>
-              </div>
-              <div>
-                  <div className="card p-5">
-                  <div className='ml-52 '><HeartIcon fill="currentColor" className="h-6 w-6 float-left text-red-600"/> 3 likes</div>
-                  {/* {product.likes.length} */}
-                      <div className="mt-80 text-2xl rounded-full w-32 h-8 text-white font-bold bg-blue-600">
-                          <div className='text-center'>{product.price}.00 ₺</div>
-                      </div>
-                  </div>
-              </div>
-        </div>
-    </div> 
   );
-};
+}
 
 export default ProductDetailItem;
